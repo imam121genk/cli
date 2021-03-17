933a70bb0d6fe06a77ac8ec921c30c892d9e450b
vilmibm <vilmibm@neongrid.space>
2021-03-17 16:35:52 -0500

    arg tests

--- pkg/cmd/workflow/view/view_test.go
+++ pkg/cmd/workflow/view/view_test.go
@@ -1,18 +1,108 @@
 package view
 
-import "testing"
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+
+	"github.com/cli/cli/pkg/cmdutil"
+	"github.com/cli/cli/pkg/iostreams"
+	"github.com/google/shlex"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestNewCmdView(t *testing.T) {
 	tests := []struct {
-		name string
-		// TODO
+		name     string
+		cli      string
+		tty      bool
+		wants    ViewOptions
+		wantsErr bool
 	}{
-		// TODO
+		// TODO web flag
+
+		{
+			name: "blank tty",
+			tty:  true,
+			wants: ViewOptions{
+				Prompt: true,
+			},
+		},
+		{
+			name:     "blank nontty",
+			wantsErr: true,
+		},
+		{
+			name: "arg tty",
+			cli:  "123",
+			tty:  true,
+			wants: ViewOptions{
+				WorkflowID: "123",
+			},
+		},
+		{
+			name: "arg nontty",
+			cli:  "123",
+			wants: ViewOptions{
+				WorkflowID: "123",
+				Raw:        true,
+			},
+		},
+		{
+			name: "web tty",
+			cli:  "--web",
+			tty:  true,
+			wants: ViewOptions{
+				Prompt: true,
+				Web:    true,
+			},
+		},
+		{
+			name: "web nontty",
+			cli:  "-w 123",
+			wants: ViewOptions{
+				Raw:        true,
+				Web:        true,
+				WorkflowID: "123",
+			},
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// TODO
+			io, _, _, _ := iostreams.Test()
+			io.SetStdinTTY(tt.tty)
+			io.SetStdoutTTY(tt.tty)
+
+			f := &cmdutil.Factory{
+				IOStreams: io,
+			}
+
+			argv, err := shlex.Split(tt.cli)
+			assert.NoError(t, err)
+
+			var gotOpts *ViewOptions
+			cmd := NewCmdView(f, func(opts *ViewOptions) error {
+				gotOpts = opts
+				return nil
+			})
+			cmd.SetArgs(argv)
+			cmd.SetIn(&bytes.Buffer{})
+			cmd.SetOut(ioutil.Discard)
+			cmd.SetErr(ioutil.Discard)
+
+			_, err = cmd.ExecuteC()
+			if tt.wantsErr {
+				assert.Error(t, err)
+				return
+			}
+
+			assert.NoError(t, err)
+
+			assert.Equal(t, tt.wants.WorkflowID, gotOpts.WorkflowID)
+			assert.Equal(t, tt.wants.Prompt, gotOpts.Prompt)
+			assert.Equal(t, tt.wants.Raw, gotOpts.Raw)
+			assert.Equal(t, tt.wants.Web, gotOpts.Web)
 		})
 	}
 }
